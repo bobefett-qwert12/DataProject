b7d377816a102414f8cfde8e329b40e46eddf86d
RyanPerkins <bobefett.qwert12@gmail.com>
2018-11-26 08:34:48 -0700

    created the data project with a gitignore

---
+++ DataProject/Model/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DataProject
+//
+//  Created by Perkins, Ryan on 11/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class ViewController: UIViewController
+{
+
+    public override func viewDidLoad() -> Void
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    public override func didReceiveMemoryWarning() -> Void
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
