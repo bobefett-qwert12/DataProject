635dfdff2cf6d42108f2372498f84b3c7d049d66
RyanPerkins <bobefett.qwert12@gmail.com>
2018-11-30 08:26:49 -0700

    fixed the controller files and added some labels to the prototype cell

--- DataProject/Controller/BucketItemCell.swift
+++ DataProject/Controller/BucketItemCell.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
-public class BucketItemCell: UITableViewCell
-{
-    
+class BucketItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
 }
