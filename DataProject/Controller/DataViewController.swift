13b0f7bae53d9942a1e02aa798b279eaf8ad7bdd
RyanPerkins <bobefett.qwert12@gmail.com>
2018-11-30 07:56:20 -0700

    added new files to handle collections, and  added the bucket18 file

---
+++ DataProject/Controller/DataViewController.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  DataProject
+//
+//  Created by Perkins, Ryan on 11/30/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class DataViewController: UITableViewController
+{
+    
+}
---
+++ DataProject/Model/BucketItem.swift
@@ -0,0 +1,14 @@
+//
+//  BucketItem.swift
+//  DataProject
+//
+//  Created by Perkins, Ryan on 11/30/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class BucketItem
+{
+    
+}
